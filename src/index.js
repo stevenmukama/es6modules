7fde00c77b1c4b607db834565dd796b617042d04
Mukama Steven <stevenmukama1@gmail.com>
2022-12-07 18:44:02 +0200

    Added Requested changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import Books from './modules/Books.js';
-import formatTs from './modules/DateTime.js';
+// import formatTs from './modules/DateTime.js';
 import { load, showOnPage } from './modules/Router.js';
+import time from './modules/luxons.js';
 
 const books = new Books((JSON.parse(localStorage.getItem('books')) === null) ? [] : JSON.parse(localStorage.getItem('books')));
 
-const dt = document.querySelector('.timer');
+const dates = document.querySelector('.timer');
 
-setInterval(() => {
-  dt.innerHTML = formatTs();
-}, 1000);
+dates.innerHTML = time();
 
 if (!window.location.hash) {
   window.location.hash = '#add';
---
+++ src/modules/luxons.js
@@ -0,0 +1,13 @@
+import { DateTime } from './luxon.js';
+
+const time = () => {
+  const { year } = DateTime.now();
+  const { month } = DateTime.now();
+  const { day } = DateTime.now();
+  const { hour } = DateTime.now();
+  const { minute } = DateTime.now();
+  const { second } = DateTime.now();
+  return `${day}/${month}/${year}, ${hour}:${minute}:${second}`;
+};
+
+export default time;
